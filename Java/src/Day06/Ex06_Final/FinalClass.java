e8d751d6ad7b481a0b26e7ad8016441cbb89897f
rlawnsrud310 <wnsrud310@gmail.com>
2024-08-12 16:06:17 +0900

    0812

---
+++ Java/src/Day06/Ex06_Final/FinalClass.java
@@ -0,0 +1,19 @@
+package Day06.Ex06_Final;
+
+public final class FinalClass {
+	public void method() {
+		System.out.println("메소드 출력!");
+	}
+	public void main(String[] args) {
+		FinalClass method = new FinalClass();
+		
+		method.method();
+	}
+}
+// final로 지정한 클래스는 상속을 할 수 없다.
+/*
+class SubClass extends FinalClass{
+	
+}
+
+*/
---
+++ Java/src/Day06/Ex06_Final/SuperClass.java
@@ -0,0 +1,16 @@
+package Day06.Ex06_Final;
+
+public class SuperClass {
+
+	// final 메소드
+	public final void finalMethod() {
+		System.out.println("final 메소드 입니다.");
+	}
+	class SubClass extends SuperClass{
+		/*
+		@Override
+		public void finalMethod() {
+			System.out.println("final 메소드는 오버라이딩 불가");
+		}*/
+	}
+}
